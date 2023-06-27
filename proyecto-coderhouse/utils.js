f61acd79b79dd60cc806036b068504f2fdb5117a
yanina glaser <yaglaser@quilmes.com.ar>
2023-06-27 16:26:36 -0300

    ejercisio 2

---
+++ proyecto-coderhouse/utils.js
@@ -0,0 +1,46 @@
+import fs from "fs";
+const fileExist = async (path) => {
+  try {
+    if (fs.existsSync(path)) {
+      return true;
+    } else {
+      return false;
+    }
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+const readFile = async (file) => {
+  try {
+    let result = await fs.promises.readFile(file, "utf-8");
+    let data = await JSON.parse(result);
+    return data;
+  } catch (err) {
+    return false;
+  }
+};
+
+const writeFile = async (file, data) => {
+  const existe = await fileExist(file);
+  if (existe === true) {
+    await deleteFile(file);
+  }
+  try {
+    await fs.promises.writeFile(file, JSON.stringify(data));
+    return true;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const deleteFile = async (file) => {
+  try {
+    await fs.promises.unlink(file);
+    return true;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export default { readFile, writeFile, deleteFile };
